3b10aac0259d34fc5aa44d23ced35311be0df6d2
devferx <ferkki@hotmail.com>
2023-12-27 10:09:56 -0400

    Add queryClient.invalidateQueries in useProductMutation

--- src/products/hooks/useProductMutation.ts
+++ src/products/hooks/useProductMutation.ts
@@ -1,14 +1,15 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { productActions } from "..";
 
 export const useProductMutation = () => {
+  const queryClient = useQueryClient();
+
   const mutation = useMutation({
     mutationFn: productActions.createProduct,
-    onSuccess: () => {
-      console.log("Product created");
-    },
-    onSettled: () => {
-      console.log("Product creation finished");
+    onSuccess: (data) => {
+      queryClient.invalidateQueries({
+        queryKey: ["products", { filterKey: data.category }],
+      });
     },
   });
 
