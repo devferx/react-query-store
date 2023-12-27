a11ab98a2d177e4b9b5ab9875a749f118e21aba3
devferx <ferkki@hotmail.com>
2023-12-27 16:14:50 -0400

    Add optimistic update in useProductMutation hook

--- src/products/hooks/useProductMutation.ts
+++ src/products/hooks/useProductMutation.ts
@@ -1,11 +1,30 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { Product, productActions } from "..";
+import { type Product, productActions } from "..";
 
 export const useProductMutation = () => {
   const queryClient = useQueryClient();
 
   const mutation = useMutation({
     mutationFn: productActions.createProduct,
+    onMutate: (product) => {
+      console.log("Mutando - Optimistic update");
+
+      const optimisticProduct: Product = {
+        ...product,
+        id: Math.random(),
+      };
+
+      queryClient.setQueryData<Product[]>(
+        ["products", { filterKey: product.category }],
+        (oldData: Product[] | undefined) => {
+          if (!oldData) return [optimisticProduct];
+
+          return [...oldData, optimisticProduct];
+        },
+      );
+
+      // Store the product in the cache of query client
+    },
     onSuccess: (product) => {
       // queryClient.invalidateQueries({
       //   queryKey: ["products", { filterKey: data.category }],
